0e39f03561b9d75d2fdb5ed83f0a4718fdd9f77d
謝忠諺 <benson8912@gmail.com>
2022-05-19 18:44:24 +0800

    0519作業

--- web410817982/Controllers/UserController.cs
+++ web410817982/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using web410817982.ViewModels;
 
 namespace web410817982.Controllers
 {
@@ -12,25 +13,25 @@ namespace web410817982.Controllers
         // GET: User
         public ActionResult SignUp()
         {
-            return View();
+            return View(new SignUpData());
         }
 
         [HttpPost]
-        public ActionResult SignUp(string name, string account, string password) 
+        public ActionResult SignUp(SignUpData data) 
         {
-            if (name.IsNullOrWhiteSpace()) {
-                ViewBag.NameMessage = "請輸入姓名";
+            //if (data.Name.IsNullOrWhiteSpace()) {
+            //    data.NameMessage = "請輸入姓名";
+            //}
+            //if (data.Account.IsNullOrWhiteSpace()) {
+            //    data.AccountMessage = "請輸入帳號";
+            //}
+            //if (data.Password.IsNullOrWhiteSpace()) {
+            //    data.PasswordMessage = "請輸入密碼";
+            //}
+            if (ModelState.IsValid) {
+                data.Message = "註冊成功";
             }
-            if (account.IsNullOrWhiteSpace()) {
-                ViewBag.AccountMessage = "請輸入帳號";
-            }
-            if (password.IsNullOrWhiteSpace()) {
-                ViewBag.PasswordMessage = "請輸入密碼";
-            }
-            if (!name.IsNullOrWhiteSpace() && !account.IsNullOrWhiteSpace() && !password.IsNullOrWhiteSpace()) {
-                ViewBag.Message = "註冊成功";
-            }
-            return View();
+            return View(data);
         }
     }
 }
